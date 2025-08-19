84bf712b325ed91eaee5bcb6b9c7a4899df9b2bb
Jadiael <jadiael@hotmail.com.br>
2025-08-19 14:29:15 -0300

    ✨ feat(utils): add formatDate function to format dates
    - create formatDate function to convert date strings to a readable format
    - use toLocaleDateString to format dates to "day month year"

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const formatDate = (dateString: string) => {
+	return new Date(dateString).toLocaleDateString('pt-BR', {
+		timeZone: 'UTC',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	});
+};
